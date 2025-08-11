23e28ae36a9481d38146f89d603ebd4809cac9dc
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-08-11 19:08:14 +0300

    create mongo.js file and write code for adding to and writting entries in phonebook in cloud-based MongoDB database

---
+++ mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const personName = process.argv[3];
+const personNumber = process.argv[4];
+
+const url = `mongodb+srv://test:${password}@cluster0.cweg3he.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (!personName && !personNumber) {
+  console.log("phonebook:");
+
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+
+    mongoose.connection.close();
+  });
+} else if (personName && personNumber) {
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  });
+  person.save().then((result) => {
+    console.log(result);
+    console.log(`added ${personName}'s number ${personNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  console.log("Please, provide both name and number for new entry");
+  mongoose.connection.close();
+}
