da0532eec85a1e63c5aceb920112f794b95fffdb
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-08-12 01:21:28 +0300

    create db configuration file person.js

---
+++ models/person.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log(result);
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("error connecting to MongoDB:", err.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
+
+module.exports = mongoose.model("Pesrson", personSchema);
